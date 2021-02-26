061f2024745ccff71f58dc67b65a9b80d6d52af4
Katzmann, Kami <Katzmann.Kami@principal.com>
2021-02-26 11:04:40 -0600

    Implemented multiple pieces for multiple people

--- src/test/java/WeekOneExercisesPartOne.java
+++ src/test/java/WeekOneExercisesPartOne.java
@@ -25,7 +25,7 @@ public class WeekOneExercisesPartOne {
 
     @Test
 
-    public void numberOfWholeSlicesFromOnePizzaAndEightPeople() {
+    public void numberOfWholeSlicesFromTwoPizzasAndFourPeople() {
         int numberOfPeople = 4;
         int numberOfEightSlicePizzas = 2;
 
@@ -47,7 +47,7 @@ public class WeekOneExercisesPartOne {
     }
 
     private int divideUpEightSlicePizzasEvenly(int numberOfPizzas, int numberOfPeople) {
-        return 1;
+        return (numberOfPizzas*8)/numberOfPeople;
     }
 
     private int determineLeftoverSlicesOfPizza(int numberOfPizzas, int numberOfPeople) {
