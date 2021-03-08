23f99a69ae7d9a2b7a5a504b374f12a0277ab3e4
Katzmann, Kami <Katzmann.Kami@principal.com>
2021-03-08 08:37:17 -0600

    Added code to check if 3 return fizz

--- src/test/java/WeekTwoExercisePartTwo.java
+++ src/test/java/WeekTwoExercisePartTwo.java
@@ -47,7 +47,7 @@ public class WeekTwoExercisePartTwo {
     }
 
     @Test
-    @Disabled
+
     public void sendingThreeShouldReturnStringFizz() {
         // TODO Comment: replace this comment with a brief description of why you made the change you did in FizzBuzz.java
         // TODO: Implement the code to make this test pass
