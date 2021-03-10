4e1045bfdf7298229732c6bc6f3836a272208ed4
Katzmann, Kami <Katzmann.Kami@principal.com>
2021-03-10 15:07:45 -0600

    Created class AuthorsAndBooks

---
+++ src/main/java/AuthorsAndBooks.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class AuthorsAndBooks {
+    String Author_Name = "Katzmann";
+    int Author_Books = 1;
+    String Book_Title = "Practicing"
+    int ISBN_Number = 978-3-16-148410-0;
+    Date Date_Published = 03/20/2000
+    int Book_Authors = 1
+
+    public String getAuthor_Name() {
+        return Author_Name;
+    }
+
+    public void setAuthor_Name(String author_Name) {
+        Author_Name = author_Name;
+        if author_Name == "Katzmann";
+            return String.valueOf(Book_Title);
+        }
+    }
+}
